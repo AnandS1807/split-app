7af544b3f77627a444aad888046efa62494a490c
AnandS1807 <mranand0709@gmail.com>
2025-06-18 00:28:59 +0530

    production stage

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgres:HZpUKcNtXZjxpxBUkQFWZvhybPUcZSyR@postgres.railway.internal:5432/railway'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:HZpUKcNtXZjxpxBUkQFWZvhybPUcZSyR@tramway.proxy.rlwy.net:46753/railway'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ENGINE_OPTIONS = {
         'pool_pre_ping': True,
